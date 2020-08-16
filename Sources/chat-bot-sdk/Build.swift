f7c43e232b3944396ca8c6f1b62e7fcd301503b5
Denis Morozov <denis.moroz.morozov@gmail.com>
2020-08-16 23:44:28 +0300

    Complete all commands

--- Sources/chat-bot-sdk/Build.swift
+++ Sources/chat-bot-sdk/Build.swift
@@ -5,7 +5,7 @@ struct Project: Decodable {
     var version: String
 }
 
-func run(botUrl: URL) throws {
+func build(botUrl: URL) throws {
     let projectUrl = botUrl.appendingPathComponent(".cbproject")
     let project = try JSONDecoder().decode(Project.self, from: try Data(contentsOf: projectUrl))
     let botname = project.name
@@ -15,6 +15,10 @@ func run(botUrl: URL) throws {
 
     let dockerUrl = botUrl.appendingPathComponent(".docker")
     clone(tag: "0.0.2", repository: repository, path: dockerUrl.path)
+
+    if FileManager.default.fileExists(atPath: dockerUrl.path) {
+        try FileManager.default.removeItem(at: dockerUrl)
+    }
 
     try FileManager.default.copyItem(
         at: botUrl.appendingPathComponent(botname),
@@ -39,4 +43,20 @@ func run(botUrl: URL) throws {
     )
 
     shell(["docker-compose", "-f", "./.docker/docker-compose.yml", "build"])
+}
+
+func up() throws {
+    shell(["docker-compose", "-f", "./.docker/docker-compose.yml", "up", "--no-start"])
+}
+
+func start() throws {
+    shell(["docker-compose", "-f", "./.docker/docker-compose.yml", "start"])
+}
+
+func stop() throws {
+    shell(["docker-compose", "-f", "./.docker/docker-compose.yml", "stop"])
+}
+
+func down() throws {
+    shell(["docker-compose", "-f", "./.docker/docker-compose.yml", "down"])
 }
--- Sources/chat-bot-sdk/main.swift
+++ Sources/chat-bot-sdk/main.swift
@@ -2,14 +2,30 @@ import Foundation
 
 let command = CommandLine.arguments[1]
 
-if command == "create" {
+if command == "help" {
+    print("commands:")
+    print("create <project_name> <version_of_tg_bot> - создать директория с проектом")
+    print("build - собрать образы")
+    print("up - собрать контейнеры")
+    print("start - запустить контейнеры")
+    print("stop - остановить контейнеры")
+    print("down - остановить и удалить контейнеры")
+} else if command == "create" {
     let baseUrl = URL(fileURLWithPath: shell(["pwd"]).first!)
     let botname = CommandLine.arguments[2]
     let tag = CommandLine.arguments[3]
     try create(baseUrl: baseUrl, botname: botname, tag: tag)
-} else if command == "run" {
+} else if command == "build" {
     let botUrl = URL(fileURLWithPath: shell(["pwd"]).first!)
-    try run(botUrl: botUrl)
+    try build(botUrl: botUrl)
+} else if command == "up" {
+    try up()
+} else if command == "start" {
+    try start()
+} else if command == "stop" {
+    try stop()
+} else if command == "down" {
+    try down()
 } else {
     throw Exeption()
 }
