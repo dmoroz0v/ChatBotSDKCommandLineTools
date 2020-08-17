566e6e4b155d6b8c0a27ee196406e882b545c914
Denis Morozov <denis.moroz.morozov@gmail.com>
2020-08-17 17:27:30 +0300

    Add verbose param

--- Sources/chat-bot-sdk/Build.swift
+++ Sources/chat-bot-sdk/Build.swift
@@ -5,7 +5,7 @@ struct Project: Decodable {
     var version: ReleaseSpec
 }
 
-func build(botUrl: URL) throws {
+func build(botUrl: URL, verbose: Bool) throws {
     let projectUrl = botUrl.appendingPathComponent(".cbproject")
     let decoder = JSONDecoder()
     decoder.keyDecodingStrategy = .convertFromSnakeCase
@@ -17,14 +17,17 @@ func build(botUrl: URL) throws {
     clone(tag: version.dockerTemplate.version, repository: version.dockerTemplate.repository, path: dockerUrl.path)
 
     if FileManager.default.fileExists(atPath: dockerUrl.path) {
+        if verbose { print("Deleting '\(dockerUrl)'") }
         try FileManager.default.removeItem(at: dockerUrl)
     }
 
+    if verbose { print("Copying '\(botUrl.appendingPathComponent(botname).path)' to '\(dockerUrl.appendingPathComponent("logic").appendingPathComponent(botname).path)'") }
     try FileManager.default.copyItem(
         at: botUrl.appendingPathComponent(botname),
         to: dockerUrl.appendingPathComponent("logic").appendingPathComponent(botname)
     )
 
+    if verbose { print("Copying '\(botUrl.appendingPathComponent("config.json").path)' to '\(dockerUrl.appendingPathComponent("logic").appendingPathComponent("config.json").path)'") }
     try FileManager.default.copyItem(
         at: botUrl.appendingPathComponent("config.json"),
         to: dockerUrl.appendingPathComponent("logic").appendingPathComponent("config.json")
@@ -35,6 +38,7 @@ func build(botUrl: URL) throws {
         "logic/App/Sources/App/main.swift",
     ]
 
+    if verbose { print("Replacing content in template") }
     try replace(
         replacingPathComponents: replacingPathComponents,
         destinationUrl: dockerUrl,
--- Sources/chat-bot-sdk/main.swift
+++ Sources/chat-bot-sdk/main.swift
@@ -2,6 +2,12 @@ import Foundation
 
 let command = CommandLine.arguments[1]
 
+func hasVerbose(args: [String]) -> Bool {
+    return nil != CommandLine.arguments.first(where: {
+        $0 == "-v"
+    })
+}
+
 if command == "help" {
     print("commands:")
     print("create <project_name> <version_of_tg_bot> - создать директория с проектом")
@@ -12,12 +18,13 @@ if command == "help" {
     print("down - остановить и удалить контейнеры")
 } else if command == "create" {
     let baseUrl = URL(fileURLWithPath: shell(["pwd"]).first!)
-    let botname = CommandLine.arguments[2]
-    let tag = CommandLine.arguments[3]
+    let arguments = CommandLine.arguments.filter { !$0.hasPrefix("-") }
+    let botname = arguments[2]
+    let tag = arguments[3]
     try create(baseUrl: baseUrl, botname: botname, tag: tag)
 } else if command == "build" {
     let botUrl = URL(fileURLWithPath: shell(["pwd"]).first!)
-    try build(botUrl: botUrl)
+    try build(botUrl: botUrl, verbose: hasVerbose(args: CommandLine.arguments))
 } else if command == "up" {
     try up()
 } else if command == "start" {
